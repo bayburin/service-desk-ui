992be991e53d9a19cac5511e1a872411cf8ec03d
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-03-25 16:05:33 +0700

    authorization: Добавил в сервис UserService метод, загружающий данные о пользователе с сервера.

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { UserI } from '@models/user.interface';
 
@@ -8,7 +11,13 @@ import { UserI } from '@models/user.interface';
 export class UserService {
   private user: UserI = JSON.parse(localStorage.getItem('currentUser'));
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  loadData(): Observable<UserI> {
+    return this.http.get('http://inv-dev/api/v1/users/info').pipe(
+      map((data: UserI) => this.user = data)
+    );
+  }
 
   getUser(): UserI {
     return this.user;
