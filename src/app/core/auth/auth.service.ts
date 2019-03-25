e508d9eaa31c806a7030e69bc802f83b3e15a5bd
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-03-25 11:03:57 +0700

    authorization: Добавил сервис авторизации AuthService.

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { TokenI } from '@models/token.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private loggedFlag: boolean = localStorage.getItem('currentToken') ? true : false;
+  private isLoggedInSub: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.loggedFlag);
+  public readonly isUserSignedIn: Observable<boolean> = this.isLoggedInSub.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<boolean> {
+    const body = {
+      grant_type: 'password',
+      username: username,
+      password: password
+    };
+
+    return this.http.post('http://inv-dev/oauth/token', body)
+      .pipe(map((token: TokenI) => {
+        if (token && token.access_token) {
+          localStorage.setItem('curerntToken', JSON.stringify(token));
+          this.isLoggedInSub.next(true);
+        }
+
+        return true;
+      }));
+  }
+
+  logout(): Observable<boolean> {
+    const body = {
+      token: JSON.parse(localStorage.getItem('currentToken')).access_token
+    };
+
+    return this.http.post('http://inv-dev/oauth/revoke', body)
+      .pipe(map(() => this.unauthorize()));
+  }
+
+  unauthorize(): boolean {
+    localStorage.removeItem('currentToken');
+    this.isLoggedInSub.next(false);
+
+    return true;
+  }
+}
