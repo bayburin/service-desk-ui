6715d16eae6727d3b749eef815c89f2a70655de4
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-04-05 13:08:02 +0700

    header: Добавил navbar первого и второго уровня.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public collapsed = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
   }
-
 }
