d71097f4b7b01563c85ca5752e7e0b2a28b4307a
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-05-27 15:36:46 +0700

    Добавлен индикатор загрузки на страницу поиска.

--- src/app/modules/ticket/pages/search/search.page.ts
+++ src/app/modules/ticket/pages/search/search.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { mergeMap, finalize } from 'rxjs/operators';
 
 import { ServiceTemplateI } from '@interfaces/service-template.interface';
 import { SearchService } from '@modules/ticket/services/search/search.service';
@@ -14,13 +14,17 @@ import { SearchService } from '@modules/ticket/services/search/search.service';
 export class SearchPageComponent implements OnInit {
   searchTerm: string;
   searchResult: Observable<ServiceTemplateI[]>;
+  loading = false;
 
   constructor(private route: ActivatedRoute, private searchService: SearchService) { }
 
   ngOnInit() {
     this.searchTerm = this.route.snapshot.queryParams.search;
     this.searchResult = this.route.queryParams.pipe(
-      mergeMap((params) => this.searchService.deepSearch(params.search))
+      mergeMap((params) => {
+        this.loading = true;
+        return this.searchService.deepSearch(params.search).pipe(finalize(() => this.loading = false));
+      })
     );
   }
 }
