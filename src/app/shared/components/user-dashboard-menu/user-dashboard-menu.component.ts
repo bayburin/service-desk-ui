dcf3220fc2c9ecbcc4521b1e809980ee82dfad3a
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-07-10 16:20:28 +0700

    Добавлен компонент user-dashboard-menu (на текущий момент с тестовыми данными).

---
+++ src/app/shared/components/user-dashboard-menu/user-dashboard-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-dashboard-menu',
+  templateUrl: './user-dashboard-menu.component.html',
+  styleUrls: ['./user-dashboard-menu.component.sass']
+})
+export class UserDashboardMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+}
