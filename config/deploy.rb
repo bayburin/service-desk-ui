11612dde558428356d9a0d38631f1336dc335f0e
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-07-17 16:37:37 +0700

    Для capistrano добавлена задача по деплою подпомулей.

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,4 +24,17 @@ namespace :ng do
   end
 end
 
+namespace :git do
+  desc 'Copy repo to releases'
+  task create_release: :'git:update' do
+    on roles(:all) do
+      with fetch(:git_environmental_variables) do
+        within repo_path do
+          execute :git, :clone, '-b', fetch(:branch), '--recursive', '.', fetch(:release_path)
+        end
+      end
+    end
+  end
+end
+
 after 'yarn:install', 'ng:build'
