268d2d7ee701e4ae19e12dd765039ac37e9f3b5d
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-07-26 17:13:32 +0700

    Добавлены тесты на StreamService.

--- src/app/shared/services/stream/stream.service.spec.ts
+++ src/app/shared/services/stream/stream.service.spec.ts
@@ -1,27 +1,43 @@
-// import { TestBed } from '@angular/core/testing';
-// import { RouterModule } from '@angular/router';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { ActionCableService } from 'angular2-actioncable';
+import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActionCableService } from 'angular2-actioncable';
 
-// import { StreamService } from './stream.service';
-// import { AppConfig, APP_CONFIG } from '@config/app.config';
-// import { AuthService } from '@auth/auth.service';
+import { StreamService } from './stream.service';
+import { AppConfig, APP_CONFIG } from '@config/app.config';
+import { AuthService } from '@auth/auth.service';
+import { environment } from 'environments/environment';
 
-// describe('StreamService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({
-//     imports: [
-//       RouterModule.forRoot([]),
-//       HttpClientTestingModule
-//     ],
-//     providers: [
-//       ActionCableService,
-//       AuthService,
-//       { provide: APP_CONFIG, useValue: AppConfig }
-//     ]
-//   }));
+describe('StreamService', () => {
+  let streamService: StreamService;
+  const accessToken = { access_token: 'my access token' };
 
-//   it('should be created', () => {
-//     const service: StreamService = TestBed.get(StreamService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterModule.forRoot([]),
+        HttpClientTestingModule
+      ],
+      providers: [
+        ActionCableService,
+        AuthService,
+        { provide: APP_CONFIG, useValue: AppConfig }
+      ]
+    });
+
+    spyOn(AuthService.prototype, 'getToken').and.returnValue(accessToken);
+    streamService = TestBed.get(StreamService);
+  });
+
+  it('should be created', () => {
+    expect(streamService).toBeTruthy();
+  });
+
+  it('should sets server url into channelServer variable', () => {
+    expect(streamService.channelServer.url).toEqual(environment.actionCableUrl);
+  });
+
+  it('should sets access token into channelServer variable', () => {
+    expect(streamService.channelServer.params).toEqual(accessToken);
+  });
+});
