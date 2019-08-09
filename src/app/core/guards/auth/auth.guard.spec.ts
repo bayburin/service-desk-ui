c492b515182ea342ff611d58bb232d27c198f66c
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-08-09 11:17:32 +0700

    Добавлены тесты на AuthGuard.

--- src/app/core/guards/auth/auth.guard.spec.ts
+++ src/app/core/guards/auth/auth.guard.spec.ts
@@ -1,15 +1,69 @@
-// import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-// import { AuthGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from '@auth/auth.service';
+import { TicketModule } from '@modules/ticket/ticket.module';
 
-// describe('AuthGuard', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [AuthGuard]
-//     });
-//   });
+class StubAuthService {
+  isLoggedInSub = new BehaviorSubject<boolean>(false);
+  isUserSignedIn = this.isLoggedInSub.asObservable();
 
-//   it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
-//     expect(guard).toBeTruthy();
-//   }));
-// });
+  setReturnUrl() {}
+  authorize() {}
+}
+
+describe('AuthGuard', () => {
+  let authService: AuthService;
+  let stubSnapshot: RouterStateSnapshot;
+  let activatedRoute: ActivatedRouteSnapshot;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TicketModule],
+      providers: [
+        AuthGuard,
+        { provide: AuthService, useClass: StubAuthService }
+      ]
+    });
+
+    authService = TestBed.get(AuthService);
+    stubSnapshot = jasmine.createSpyObj<RouterStateSnapshot>('RouterStateSnapshot', ['url']);
+    activatedRoute = new ActivatedRouteSnapshot();
+  });
+
+  it('should create', inject([AuthGuard], (guard: AuthGuard) => {
+    expect(guard).toBeTruthy();
+  }));
+
+  it('should return true if user logged in', inject([AuthGuard], (guard: AuthGuard) => {
+    authService.isLoggedInSub.next(true);
+
+    guard.canActivate(activatedRoute, stubSnapshot).subscribe(result => {
+      expect(result).toBeTruthy();
+    });
+  }));
+
+  describe('when user does not logged in', () => {
+    it('should call "setReturnUrl" method for AuthService', inject([AuthGuard], (guard: AuthGuard) => {
+      spyOn(authService, 'setReturnUrl');
+      guard.canActivate(activatedRoute, stubSnapshot).subscribe(() => {
+        expect(authService.setReturnUrl).toHaveBeenCalledWith(stubSnapshot.url);
+      });
+    }));
+
+    it('should call "authorize" method for AuthService', inject([AuthGuard], (guard: AuthGuard) => {
+      spyOn(authService, 'authorize');
+      guard.canActivate(activatedRoute, stubSnapshot).subscribe(() => {
+        expect(authService.authorize).toHaveBeenCalled();
+      });
+    }));
+
+    it('should return false', inject([AuthGuard], (guard: AuthGuard) => {
+      guard.canActivate(activatedRoute, stubSnapshot).subscribe(result => {
+        expect(result).toBeFalsy();
+      });
+    }));
+  });
+});
--- src/app/core/guards/auth/auth.guard.ts
+++ src/app/core/guards/auth/auth.guard.ts
@@ -1,26 +1,17 @@
-import { Injectable, Inject } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { APP_CONFIG } from '@config/app.config';
-import { AppConfigI } from '@interfaces/app-config.interface';
 import { AuthService } from '@auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    @Inject(APP_CONFIG) private config: AppConfigI,
-  ) {}
+  constructor(private authService: AuthService) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return this.authService.isUserSignedIn
       .pipe(map((isLoggedIn: boolean) => {
         if (isLoggedIn) {
