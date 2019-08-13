d479c9506f252d33e348b540f5a35841c40e28ef
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-08-13 15:34:47 +0700

    Добавлены тесты на CategoryHeaderComponent.

--- src/app/modules/ticket/components/category-header/category-header.component.spec.ts
+++ src/app/modules/ticket/components/category-header/category-header.component.spec.ts
@@ -1,25 +1,37 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// import { CategoryHeaderComponent } from './category-header.component';
+import { CategoryHeaderComponent } from './category-header.component';
+import { CategoryFactory } from '@modules/ticket/factories/category.factory';
+import { Category } from '@modules/ticket/models/category.model';
 
-// describe('CategoryHeaderComponent', () => {
-//   let component: CategoryHeaderComponent;
-//   let fixture: ComponentFixture<CategoryHeaderComponent>;
+describe('CategoryHeaderComponent', () => {
+  let component: CategoryHeaderComponent;
+  let fixture: ComponentFixture<CategoryHeaderComponent>;
+  let category: Category;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ CategoryHeaderComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [CategoryHeaderComponent]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CategoryHeaderComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CategoryHeaderComponent);
+    component = fixture.componentInstance;
+    category = CategoryFactory.create({ id: 1, name: 'Тестовая категория' });
+    component.category = category;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show link to category details', () => {
+    expect(fixture.debugElement.nativeElement.textContent).toContain(category.name);
+    expect(fixture.debugElement.nativeElement.querySelector('a[href="/categories/1"]')).toBeTruthy();
+  });
+});
