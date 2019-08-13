f0e46a670d427514ad70422287aa8335c5a5a23c
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-08-13 10:41:57 +0700

    Добавлены тесты на CasesPageComponent.

--- src/app/modules/case/pages/cases/cases.page.spec.ts
+++ src/app/modules/case/pages/cases/cases.page.spec.ts
@@ -1,25 +1,31 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-// import { CasesPageComponent } from './cases.page';
+import { CasesPageComponent } from './cases.page';
 
-// describe('CasesPageComponent', () => {
-//   let component: CasesPageComponent;
-//   let fixture: ComponentFixture<CasesPageComponent>;
+describe('CasesPageComponent', () => {
+  let component: CasesPageComponent;
+  let fixture: ComponentFixture<CasesPageComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ CasesPageComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CasesPageComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CasesPageComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CasesPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render app-cases-page-content component', () => {
+    expect(fixture.debugElement.nativeElement.querySelector('app-cases-page-content')).toBeTruthy();
+  });
+});
