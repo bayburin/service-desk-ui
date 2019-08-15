6788172d13dfa003f172995c33869fb07664a241
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-08-15 10:05:59 +0700

    Добавлены тесты на ServiceTemplateCreator

---
+++ src/app/modules/ticket/core/service_template_creator/service_template_creator.spec.ts
@@ -0,0 +1,40 @@
+import { ServiceTemplateCreator } from './service_template_creator';
+import { Ticket } from '@modules/ticket/models/ticket/ticket.model';
+import { Service } from '@modules/ticket/models/service.model';
+import { Category } from '@modules/ticket/models/category.model';
+import { ServiceTemplateI } from '@interfaces/service-template.interface';
+
+describe('ServiceTemplateCreator', () => {
+  it('should call TicketFactory', () => {
+    const object = {
+      id: 1,
+      name: 'Тестовый услуга',
+      service_id: 1,
+      ticket_type: 'question'
+    } as ServiceTemplateI;
+    const result = ServiceTemplateCreator.createBy(object);
+
+    expect(result instanceof Ticket).toBeTruthy();
+  });
+
+  it('should call ServiceFactory', () => {
+    const object = {
+      id: 1,
+      name: 'Тестовый вопрос',
+      category_id: 1,
+    } as ServiceTemplateI;
+    const result = ServiceTemplateCreator.createBy(object);
+
+    expect(result instanceof Service).toBeTruthy();
+  });
+
+  it('should call CategoryFactory', () => {
+    const object = {
+      id: 1,
+      name: 'Тестовый вопрос',
+    } as ServiceTemplateI;
+    const result = ServiceTemplateCreator.createBy(object);
+
+    expect(result instanceof Category).toBeTruthy();
+  });
+});
