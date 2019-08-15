61f3676e5c469e405cc9dc5ab31b49ea2b69336d
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-08-15 15:43:32 +0700

    Добавлены тесты на CategoriesPageComponent.

--- src/app/modules/ticket/pages/categories/categories.page.spec.ts
+++ src/app/modules/ticket/pages/categories/categories.page.spec.ts
@@ -1,25 +1,32 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-// import { CategoriesPageComponent } from './categories.page';
+import { CategoriesPageComponent } from './categories.page';
 
-// describe('CategoriesPageComponent', () => {
-//   let component: CategoriesPageComponent;
-//   let fixture: ComponentFixture<CategoriesPageComponent>;
+describe('CategoriesPageComponent', () => {
+  let component: CategoriesPageComponent;
+  let fixture: ComponentFixture<CategoriesPageComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ CategoriesPageComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, NoopAnimationsModule],
+      declarations: [CategoriesPageComponent]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CategoriesPageComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CategoriesPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render router-outlet component', () => {
+    expect(fixture.debugElement.nativeElement.querySelector('router-outlet')).toBeTruthy();
+  });
+});
