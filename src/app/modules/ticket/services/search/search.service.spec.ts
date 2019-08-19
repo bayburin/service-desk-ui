6b68173d23e617105c232e09e125e5744437d063
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-08-19 10:36:14 +0700

    Добавлены тесты на SearchService.

--- src/app/modules/ticket/services/search/search.service.spec.ts
+++ src/app/modules/ticket/services/search/search.service.spec.ts
@@ -1,12 +1,75 @@
-// import { TestBed } from '@angular/core/testing';
+import { environment } from 'environments/environment';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpParams } from '@angular/common/http';
 
-// import { SearchService } from './search.service';
+import { SearchService } from './search.service';
+import { SearchSortingPipe } from '@shared/pipes/search-sorting/search-sorting.pipe';
+import { CategoryFactory } from '@modules/ticket/factories/category.factory';
+import { ServiceFactory } from '@modules/ticket/factories/service.factory';
+import { TicketFactory } from '@modules/ticket/factories/ticket.factory';
 
-// describe('SearchService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+describe('SearchService', () => {
+  let searchService: SearchService;
+  let httpTestingController: HttpTestingController;
+  const serverData = [
+    { id: 1, name: 'Тестовая категория' },
+    { id: 2, name: 'Тестовая услуга', category_id: 1 },
+    { id: 3, name: 'Тестовый вопрос', service_id: 2, ticket_type: 'question' }
+  ];
+  const expectedResult = [
+    CategoryFactory.create({ id: 1, name: 'Тестовая категория' }),
+    ServiceFactory.create({ id: 2, name: 'Тестовая услуга', category_id: 1 }),
+    TicketFactory.create({ id: 3, name: 'Тестовый вопрос', service_id: 2, ticket_type: 'question' })
+  ];
+  const searchStr = 'search string';
+  const params = new HttpParams().set('search', searchStr);
 
-//   it('should be created', () => {
-//     const service: SearchService = TestBed.get(SearchService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [SearchSortingPipe]
+    });
+
+    searchService = TestBed.get(SearchService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should be created', () => {
+    expect(SearchService).toBeTruthy();
+  });
+
+  describe('#search', () => {
+    const searchUri = `${environment.serverUrl}/api/v1/dashboard/search`;
+
+    it('should return Observable with search data', () => {
+      searchService.search(searchStr).subscribe(result => {
+        expect(result).toEqual(expectedResult);
+      });
+
+      httpTestingController.expectOne({
+        method: 'GET',
+        url: `${searchUri}?${params}`
+      }).flush(serverData);
+    });
+  });
+
+  describe('#deepSearch', () => {
+    const searchUri = `${environment.serverUrl}/api/v1/dashboard/deep_search`;
+
+    it('should return Observable with search data', () => {
+      searchService.deepSearch(searchStr).subscribe(result => {
+        expect(result).toEqual(expectedResult);
+      });
+
+      httpTestingController.expectOne({
+        method: 'GET',
+        url: `${searchUri}?${params}`
+      }).flush(serverData);
+    });
+  });
+});
