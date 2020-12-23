277210100fae996d379575be4c166f7d48d43c18
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-12-23 10:17:59 +0700

    Добавлен snow-flake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,6 +34,7 @@ export class AppComponent implements OnInit, AfterViewChecked {
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => this.location = event.urlAfterRedirects.split('?')[0]);
     this.isUserSignedIn = this.authService.isUserSignedIn;
+    this.initSnowflakes();
   }
 
   ngAfterViewChecked() {
@@ -62,5 +63,20 @@ export class AppComponent implements OnInit, AfterViewChecked {
       }
       iframe.remove();
     }, 1000);
+  }
+
+  private initSnowflakes() {
+    const Snowflakes = require('magic-snowflakes');
+    const sf = new Snowflakes({
+      container: document.querySelector('body'), // Default: document.body
+      count: 10, // 100 snowflakes. Default: 50
+      minOpacity: 0.1, // From 0 to 1. Default: 0.6
+      maxOpacity: 0.95, // From 0 to 1. Default: 1
+      minSize: 10, // Default: 8
+      maxSize: 30, // Default: 18
+      rotation: true, // Default: true
+      speed: 1, // The property affects the speed of falling. Default: 1
+      wind: false, // Without wind. Default: true
+    });
   }
 }
